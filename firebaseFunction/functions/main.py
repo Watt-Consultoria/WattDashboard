35e8d6ae714a777565c0e49df4a4e5866936e1a7
Gabriel de Oliveira e Silva <gabriel.gos@ufpe.br>
2026-02-23 11:29:48 -0300

    delete: firebaseFunction

--- firebaseFunction/functions/main.py
+++
@@ -1,7 +0,0 @@
-from firebase_functions.options import set_global_options
-
-from psel_email_confirmation import send_psel_confirmation_email
-from remake import scheduled_task
-
-# Cost control for this codebase.
-set_global_options(max_instances=10)
