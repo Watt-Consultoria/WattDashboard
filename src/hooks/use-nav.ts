dfd84dea1fdb2925fdebe5fed0895ed9e8a1b126
yanlima <yangclima@gmail.com>
2026-03-03 19:54:29 -0300

    fix: `use-nav`

--- src/hooks/use-nav.ts
+++ src/hooks/use-nav.ts
@@ -101,13 +101,16 @@ export function useFilteredNavItems(items: NavItem[]) {
       allowedUrls.add('/dashboard/faltas');
     }
 
+    const allowEstatisticas = ['diretor', 'presidente', 'assessor'].includes(
+      role
+    );
+    if (allowEstatisticas) {
+      allowedUrls.add('/dashboard/estatisticas');
+    }
+
     const allowHog = true;
     if (allowHog) {
       allowedUrls.add('/dashboard/hogwatts');
-      
-    const allowEstatisticas = ['diretor', 'presidente', 'assessor'].includes(role);
-    if (allowEstatisticas) {
-      allowedUrls.add('/dashboard/estatisticas');
     }
 
     const allowFinanceiro = ['presidente', 'assessor'].includes(role);
